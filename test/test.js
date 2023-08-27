ad66108a18863aebe100c702cccd34d719a0dfb8
Kzzmz <demyantkachev2018@gmail.com>
2023-08-27 23:19:48 +0300

    test: add tests

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const { parseTime, deparseTime } = require("../dist/cjs/index.js");
+
+console.log(`parseTime("1day5hour9min").ms parseTime("1day5hour9min").hours parseTime("1day5hour9min")._deparsed\n\t${parseTime("1day5hour9min").ms} ${parseTime("1day5hour9min").hours} ${parseTime("1day5hour9min")._deparsed}\n\t104940000 29.15 1d5h9m [correct]`);
+console.log(`parseTime("") parseTime("ms") parseTime("0y") parseTime("99999999y")\n\t${parseTime("")} ${parseTime("ms")} ${parseTime("0y")} ${parseTime("99999999y")}\n\tnull null null null [correct]`);
+console.log(`deparseTime(104940000)\n\t${deparseTime(104940000)}\n\t1d5h9m [correct]`);
+
+/* RESULT:
+parseTime("1day5hour9min").ms parseTime("1day5hour9min").hours parseTime("1day5hour9min")._deparsed
+	104940000 29.15 1d5h9m
+	104940000 29.15 1d5h9m [correct]
+parseTime("") parseTime("ms") parseTime("0y") parseTime("99999999y")
+	null null null null
+	null null null null [correct]
+deparseTime(104940000)
+	1d5h9m
+	1d5h9m [correct]
+*/
